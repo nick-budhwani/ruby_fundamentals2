d391de0d1d02d4d24d3d489eac17b2f31e4515d3
nadir budhwani <deniro0@yahoo.com>
2016-01-13 16:03:37 -0500

    made changes to the method

--- exercise6.rb
+++ exercise6.rb
@@ -1,28 +1,20 @@
-# def list
-#   grocery_list = ["juice", "paper towels", "nutella", "nuggets"]
-#   grocery_list.each do |list|
-#     puts "*#{list}"
-# end
-#
-#
-#
-# # grocery_list.each do |list|
-# #   puts "*#{list}"
-# # end
-# #
-# # grocery_list << "rice"
-# # grocery_list.each do |list|
-# #   puts "*#{list}"
-# # end
-#
-# list
-
 grocery_list = ["juice", "paper towels", "nutella", "nuggets"]
 grocery_list << "rice"
-def list_output(grocery_list)
-  grocery_list.each do |list|
-    puts "*#{list}"
+
+def list_output(list)
+  list.each do |show|
+    puts "*#{show}"
   end
 end
 
 list_output(grocery_list)
+
+puts "There are #{grocery_list.length} items in the list."
+
+if grocery_list == grocery_list.include?("bananas")
+  puts "You need to pick up bananas."
+else
+  puts "You don\'t need to pick up bananas today."
+end
+
+puts grocery_list[1]
