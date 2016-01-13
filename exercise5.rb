80ed496e35d831d48ccd7a65af6a41fdc98e92c6
nadir budhwani <deniro0@yahoo.com>
2016-01-13 14:31:44 -0500

    made changes, made changes back

--- exercise5.rb
+++ exercise5.rb
@@ -2,7 +2,7 @@ def temp_conversion
   puts "What is the temperature in Farenheit?"
   tempf = gets.chomp.to_i
   tempc = (tempf - 32) * 5/9
-  puts "#{tempf} Farenheit is #{tempc} Celsius"
+  puts "#{tempf} Farenheit is #{tempc} Celsius."
 end
 
 temp_conversion
